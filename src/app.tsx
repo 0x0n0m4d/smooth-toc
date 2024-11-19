d81ad0ecfa22964288b08a4331783643a5dc239d
d3m0n <th3l1ghtd3m0n@gmail.com>
2024-11-19 05:56:13 -0300

    feat(ui): add sidebar

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import '@/app.css';
+import { Box, HStack } from '@kuma-ui/core';
 import { Container } from './atoms/container';
 import { MainContent } from './components/main-content';
 import { MarkdowView } from './components/markdown-view';
@@ -9,7 +10,26 @@ function App() {
     <Container>
       <MainContent>
         <PageTitle />
-        <MarkdowView />
+        <HStack gap={[0, 0, 0, '1em', '2em']}>
+          <Box flexGrow={1} minWidth={0}>
+            <MarkdowView />
+          </Box>
+          <Box
+            display={['none', 'none', 'block', 'block']}
+            flexGrow={0}
+            flexShrink={0}
+            flexBasis={[0, 0, 200, 200, 300]}
+          >
+            <Box
+              position="sticky"
+              top={16}
+              maxHeight="calc(100vh - 70px)"
+              overflow="auto"
+            >
+              Outline goes here
+            </Box>
+          </Box>
+        </HStack>
       </MainContent>
     </Container>
   );
