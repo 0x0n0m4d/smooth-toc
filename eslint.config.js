181d78da6b2d555e98a10119313e6b11faf286e8
d3m0n <th3l1ghtd3m0n@gmail.com>
2024-11-18 23:28:24 -0300

    style(*): renaming

--- eslint.config.js
+++ eslint.config.js
@@ -5,32 +5,33 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-    { ignores: ['dist'] },
-    {
-        extends: [js.configs.recommended, ...tseslint.configs.recommended],
-        files: ['**/*.{ts,tsx}'],
-        languageOptions: {
-            ecmaVersion: 2020,
-            globals: globals.browser
-        },
-        plugins: {
-            'react-hooks': reactHooks,
-            'react-refresh': reactRefresh
-        },
-        rules: {
-            ...reactHooks.configs.recommended.rules,
-            '@typescript-eslint/no-explicity-any': 'off',
-            '@typescript-eslint/no-empty-object-type': 'off',
-            '@typescript-eslint/no-unused-vars': [
-                'warn', {
-                    argsIgnorePattern: '^_',
-                    varsIgnorePattern: '^_'
-                }
-            ],
-            'react-refresh/only-export-components': [
-                'warn',
-                { allowConstantExport: true }
-            ]
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/no-explicity-any': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_'
         }
+      ],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true }
+      ]
     }
+  }
 );
